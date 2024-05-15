5a234784be0935307cd75a5740f3ef636313c5fe
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2024-05-16 01:48:50 +0530

    2nd

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,7 +1,14 @@
+import CIcon from '@coreui/icons-react';
+import { cilList  } from '@coreui/icons';
+import "./card.css"
 
 export function Card(params) {
     return (
-        <h1></h1>
+        <div className='border'>
+            <CIcon icon={cilList} className='icon'/>
+            <p className='titleClass'>{params.title}</p>
+            <p className='descriptionClass'>{params.description}</p>
+        </div>
     )
     
 }
